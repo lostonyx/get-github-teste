7971ae14fc97462050887451741f8192e3426a96
LostOnyx <github@lostonyx.com.br>
2020-08-14 18:03:48 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,23 +1,40 @@
 function loopUsers(reply) {
     $("#userTable tbody tr").remove();
-    document.getElementById("userTable").style.display = "initial";
-    document.getElementById("reposTable").style.display = "none";
     for(var i = 0; i < reply.length; i++) {
         var user = reply[i];
         var avatar = user.avatar_url.replace("https", "http");
         $("#userTable").find('tbody').append('<tr><td><img class="image" src="' + avatar + '" /></td><td>' + user.login + '</td><td><a target="_blank" href="' + user.html_url + '">' + user.html_url + '</a></td><td><button class="btn btn-success" onclick="getRepos(\'' + user.login + '\')">Repositórios</td></tr>');
-
     }
 }
 
 function loopRepos(reply) {
     $("#reposTable tbody tr").remove();
-    document.getElementById("userTable").style.display = "none";
-    document.getElementById("reposTable").style.display = "initial";
     for(var i = 0; i < reply.length; i++) {
         var repo = reply[i];
         $("#reposTable").find('tbody').append('<tr><td>' + repo.name + '</td><td><a target="_blank" href="' + repo.html_url + '">' + repo.html_url + '</a></td></tr>');
     }
+}
+
+function listAllUsers(button) {
+    
+    var request = new XMLHttpRequest();
+    if(document.getElementById('usersPageMore').dataset.page) {
+        page = document.getElementById('usersPageMore').dataset.page;
+    } else {
+        page = 0;
+    }
+    request.open('get', 'http://api.github.com/users?since=' + page, true);
+    request.onload = function() {
+        const reply = JSON.parse(this.responseText);
+        lastId = reply[reply.length-1].id;
+        document.getElementById('usersPageMore').dataset.page = lastId+1;
+        document.getElementById("reposTable").style.display = "none";
+        document.getElementById("divAllRepos").style.display = "none";
+        document.getElementById("userTable").style.display = "initial";
+        document.getElementById("divAllUsers").style.display = "initial";
+        loopUsers(reply);
+    }
+    request.send();
 }
 
 function searchUser(user) {
@@ -25,7 +42,47 @@ function searchUser(user) {
     request.open('get', 'http://api.github.com/search/users?q=' + user, true);
     request.onload = function() {
         const reply = JSON.parse(this.responseText);
+        document.getElementById("userTable").style.display = "initial";
+        document.getElementById("divAllUsers").style.display = "none";
+        document.getElementById("reposTable").style.display = "none";
+        document.getElementById("divAllRepos").style.display = "none";
         loopUsers(reply.items);
+    };
+    request.send();
+}
+
+function listAllRepos() {
+    
+    var request = new XMLHttpRequest();
+    if(document.getElementById('reposPageMore').dataset.page) {
+        page = document.getElementById('reposPageMore').dataset.page;
+    } else {
+        page = 0;
+    }
+    request.open('get', 'http://api.github.com/repositories?since=' + page, true);
+    request.onload = function() {
+        const reply = JSON.parse(this.responseText);
+        lastId = reply[reply.length-1].id;
+        document.getElementById('reposPageMore').dataset.page = lastId+1;
+        document.getElementById("reposTable").style.display = "initial";
+        document.getElementById("divAllRepos").style.display = "initial";
+        document.getElementById("userTable").style.display = "none";
+        document.getElementById("divAllUsers").style.display = "none";
+        loopRepos(reply);
+    }
+    request.send();
+}
+
+function searchRepo(repo) {
+    var request = new XMLHttpRequest();
+    request.open('get', 'http://api.github.com/search/repositories?q=' + repo, true);
+    request.onload = function() {
+        const reply = JSON.parse(this.responseText);
+        document.getElementById("userTable").style.display = "none";
+        document.getElementById("divAllUsers").style.display = "none";
+        document.getElementById("reposTable").style.display = "initial";
+        document.getElementById("divAllRepos").style.display = "none";
+        loopRepos(reply.items);
     };
     request.send();
 }
@@ -35,15 +92,22 @@ function getRepos(user) {
     request.open('get', 'http://api.github.com/users/' + user + '/repos', true);
     request.onload = function() {
         const reply = JSON.parse(this.responseText);
-        loopRepos(reply)
+        loopRepos(reply);
     };
     request.send();
 }
 
 document.getElementById("userTable").style.display = "none";
 document.getElementById("reposTable").style.display = "none";
+document.getElementById("divAllUsers").style.display = "none";
+document.getElementById("divAllRepos").style.display = "none";
 
 $('#searchUser').submit(function() {
     user = $('#user').val();
     searchUser(user);
+});
+
+$('#searchRepo').submit(function() {
+    repo = $('#repo').val();
+    searchRepo(repo);
 });
